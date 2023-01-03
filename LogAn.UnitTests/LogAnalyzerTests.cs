1d25ac3e3267fcb8be0ea82117efcbc5e260b3d5
HowardHuang <alltop3190@gmail.com>
2023-01-03 22:32:58 +0800

    加入專案檔案。

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtenstion_ReturnFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithbadextenstion.foo");
+            Assert.IsFalse(result);
+        }
+    }
+}
