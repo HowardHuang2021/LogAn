062051719dd8ea148da6e87c426aeb12a101869f
HowardHuang <alltop3190@gmail.com>
2023-01-03 23:11:17 +0800

    重構 new LogAnalyzer 使用工廠方法

--- LogAn.UnitTests/LogAnalyzerTests.cs
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -10,9 +10,13 @@ namespace LogAn.UnitTests
         [TestCase("filewithbadextenstion.foo", false)]
         public void IsValidLogFileName_VariousExtenstions_ChecksThem(string file, bool expected)
         {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName(file);
+            LogAnalyzer la = MakeAnalyzer();
+            bool result = la.IsValidLogFileName(file);
             Assert.AreEqual(expected, result);
+        }
+        private LogAnalyzer MakeAnalyzer()
+        {
+            return new LogAnalyzer();
         }
     }
 }
