783d6c982bfecd8e0e50b3899ee06fdf64649364
HowardHuang <alltop3190@gmail.com>
2023-01-06 23:34:55 +0800

    間單的模擬物件練習

---
+++ LogAnMockWebService.UnitTests/LogAnMockWebServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LogAnMockWebService.UnitTests
+{
+    [TestFixture]
+    public class LogAnMockWebServiceTests
+    {
+        [Test]
+        public void Analyze_TooShortFileName_CallsWebService()
+        {
+            FakeWebService mockWebService = new FakeWebService();
+            LogAnalyzer log = new LogAnalyzer(mockWebService);
+            string tooShortFileName = "abc.ext";
+            log.Analyzer(tooShortFileName);
+            StringAssert.Contains("Filename too short:abc.ext", mockWebService.LastError);
+        }
+    }
+}
---
+++ LogAnMockWebService/LogAnMockWebService.cs
@@ -0,0 +1,31 @@
+﻿namespace LogAnMockWebService
+{
+    public interface IWebService
+    {
+        void LogError(string message);
+    }
+    public class FakeWebService : IWebService
+    {
+        public string LastError;
+        public void LogError(string message)
+        {
+            LastError = message;
+        }
+    }
+    public class LogAnalyzer
+    {
+        private IWebService service;
+        public LogAnalyzer(IWebService service)
+        {
+            this.service = service;
+        }
+        public void Analyzer(string fileName)
+        {
+            if (fileName.Length < 8)
+            {
+                service.LogError("Filename too short:"
+                    + fileName);
+            }
+        }
+    }
+}
