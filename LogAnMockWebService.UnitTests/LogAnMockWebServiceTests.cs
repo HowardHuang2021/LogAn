79a8948a767756040fb7abeaf9c1253a486c2606
HowardHuang <alltop3190@gmail.com>
2023-01-07 00:06:22 +0800

    模擬 Email 服務是否有被正確呼叫

--- LogAnMockWebService.UnitTests/LogAnMockWebServiceTests.cs
+++ LogAnMockWebService.UnitTests/LogAnMockWebServiceTests.cs
@@ -15,4 +15,21 @@ namespace LogAnMockWebService.UnitTests
             StringAssert.Contains("Filename too short:abc.ext", mockWebService.LastError);
         }
     }
+    [TestFixture]
+    public class LogAnalyzer2Tests
+    {
+        [Test]
+        public void Analyzer_WebServiceThrows_SendsEmail()
+        {
+            FakeWebService mockWebService = new FakeWebService();
+            mockWebService.ToThrow = new Exception("fake exception");
+            FakeEmailService mockEmail = new FakeEmailService();
+            LogAnalyzer2 log = new LogAnalyzer2(mockWebService, mockEmail);
+            string tooShortFileName = "abc.ext";
+            log.Analyzer(tooShortFileName);
+            StringAssert.Contains("someone@somewhere.com", mockEmail.To);
+            StringAssert.Contains("can't log", mockEmail.Subject);
+            StringAssert.Contains("fake exception", mockEmail.Body);
+        }
+    }
 }
--- LogAnMockWebService/LogAnMockWebService.cs
+++ LogAnMockWebService/LogAnMockWebService.cs
@@ -7,8 +7,13 @@
     public class FakeWebService : IWebService
     {
         public string LastError;
+        public Exception ToThrow;
         public void LogError(string message)
         {
+            if (ToThrow != null)
+            {
+                throw ToThrow;
+            }
             LastError = message;
         }
     }
@@ -23,8 +28,55 @@
         {
             if (fileName.Length < 8)
             {
-                service.LogError("Filename too short:"
-                    + fileName);
+                service.LogError("Filename too short:" + fileName);
+            }
+        }
+    }
+    public interface IEmailService
+    {
+        void SendEmail(string to, string subject, string body);
+    }
+    public class FakeEmailService : IEmailService
+    {
+        public string To;
+        public string Subject;
+        public string Body;
+        public void SendEmail(string to, string subject, string body)
+        {
+            To = to;
+            Subject = subject;
+            Body = body;
+        }
+    }
+    public class LogAnalyzer2
+    {
+        public LogAnalyzer2(IWebService service, IEmailService email)
+        {
+            Service = service;
+            Email = email;
+        }
+        public IWebService Service
+        {
+            get;
+            set;
+        }
+        public IEmailService Email
+        {
+            get;
+            set;
+        }
+        public void Analyzer(string fileName)
+        {
+            if (fileName.Length < 8)
+            {
+                try
+                {
+                    Service.LogError("Filename too short:" + fileName);
+                }
+                catch (Exception ex)
+                {
+                    Email.SendEmail("someone@somewhere.com", "can't log", ex.Message);
+                }
             }
         }
     }
